80c0d46e3b88ca08ff31549bb98d03c751cbfbc2
Xander C <93937517+Xander-Carroll@users.noreply.github.com>
2023-07-19 21:37:02 -0400

    Roll Button on Item Sheets.

--- scripts/item-sheet.js
+++ scripts/item-sheet.js
@@ -1,6 +1,8 @@
 //Extends the basic ItemSheet with some very simple modifications
 
- export class XandersSwnItemSheet extends ItemSheet {
+import { useItem } from "./utils.js";
+
+export class XandersSwnItemSheet extends ItemSheet {
 
     //@override
     static get defaultOptions() {
@@ -20,6 +22,12 @@
     activateListeners(html){
         super.activateListeners(html);
 
+        //Return if the player can't edt the sheet.
+        if (!this.isEditable) return;
+
+        //If an item is clicked.
+        html.find('.item-image-clickable').on("click", this._onItemUse.bind(this));
+
     }
 
     //@override
@@ -34,8 +42,14 @@
         //Uncomment this line to see what data can be accsessed in the handelbars sheet.
         //console.log(context);
     
-        if (typeof context.system.type === 'undefined'){
+        //Weapon type is not a property for the core system, so it needs to be added.
+        if (context.type === "weapon" && typeof context.system.type === 'undefined'){
             context.system.type = "melee";
+        }
+
+        //If the item is in a compendium, its buttons shouldn't be active.
+        if(context.item.compendium){
+            context.owner = false;
         }
 
         return context;
@@ -44,6 +58,29 @@
     //@override
     get template() {
         return `modules/xanders-swnr-sheet/scripts/templates/items/item-${this.item.type}-sheet.html`;
+    }
+
+    //Called when an item's roll symbol is clicked. (When the item should be used).
+    async _onItemUse(event){
+        event.preventDefault();
+
+        //If the user is not an owner of the item, they shouldn't be abe to roll it.
+        if(!this.object.isOwner) return;
+
+        //If the item is an embedded item, use that actor, if it is not an embedded item, use the current token.
+        let actor = ChatMessage.getSpeaker().actor;        
+        if(this.object.isEmbedded){
+            actor = this.actor;
+        }
+        
+        //If the item is not embedded and no token is selected, give a warning.
+        if(!actor){
+            ui.notifications.warn("You must have a token selected to use this item!");
+            return;
+        }
+
+        //Use the item with the given actor.
+        useItem(this.object, actor);
     }
 
 }
